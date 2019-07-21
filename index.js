c9b4dccd84778427076d971c7fdfabcec7de5bdf
Mario Carrasquero <mariocarrasquero@Marios-MacBook-Pro.local>
2019-07-21 14:25:06 -0500

    asd

--- index.js
+++ index.js
@@ -18,59 +18,6 @@ const {
 const PORT = process.env.PORT || 5000
 
 
-
-
-// var url = "https://api.mercadolibre.com/shipment_labels?shipment_ids=28018420154&savePdf=Y&access_token=APP_USR-2296962313426801-072116-fb9504672f3b683db092c8cce1e1e7b8-288767758"
- 
-// var options = {
-//     directory: "./guias/",
-//     filename: "123.pdf"
-// }
- 
-// download(url, options, function(err){
-//     if (err) throw err
-//     console.log("meow")
-
-//     const assets = {
-//       guiaEnvioBytes: fs.readFileSync('./guias/123.pdf'),
-//     };
-//     const pdfDoc = PDFDocumentFactory.load(assets.guiaEnvioBytes);
-    
-//     pdfDoc.removePage(0)
-//     const pdfBytes = PDFDocumentWriter.saveToBytes(pdfDoc);
-    
-//     /* ========================== 7. Write PDF to File ========================== */
-//     // This step is platform dependent. Since this is a Node script, we can just
-//     // save the `pdfBytes` to the file system, where it can be opened with a PDF
-//     // reader.
-//     const filePath = `${__dirname}/guias/123.pdf`;
-//     fs.writeFileSync(filePath, pdfBytes);
-//     console.log(`PDF file written to: ${filePath}`);
-    
-    
-    
-    
-//     const optionsPrinter = {
-//       printer: "M2070",
-//       copies: 3,
-//       copie: 3
-//     };
-    
-//     printer
-//      .print("./guias/123.pdf",optionsPrinter)
-//       .then(console.log)
-//       .catch(console.error);
-// }) 
-
-
-
-
-
-
-
-
-
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -98,8 +45,13 @@ download(url, options, function(err){
 
       //ya se decargo 
       //ahora modificar 
+      const arichivo = './guias/'+idVenta+'.pdf';
+      var direccionArchivo = path.dirname(arichivo);
+      
+      
+      console.log( path.resolve('./guias/'+idVenta+'.pdf'));
           const assets = {
-          guiaEnvioBytes: fs.readFileSync('./guias/'+idVenta+'.pdf'),
+          guiaEnvioBytes: fs.readFileSync(path.resolve('./guias/'+idVenta+'.pdf')),
         };
         const pdfDoc = PDFDocumentFactory.load(assets.guiaEnvioBytes);
         
@@ -117,7 +69,7 @@ download(url, options, function(err){
           };
           
           printer
-          .print("./guias/"+idVenta+".pdf",optionsPrinter)
+          .print(path.resolve('./guias/'+idVenta+'.pdf'),optionsPrinter)
           .then(function(value) {
             res.send({ 
               status: '200',
