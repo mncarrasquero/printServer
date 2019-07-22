a437c344a1f557bdace743df1f917c666ea738b8
Mario Carrasquero <mariocarrasquero@Marios-MacBook-Pro.local>
2019-07-21 19:01:50 -0500

    asd

--- index.js
+++ index.js
@@ -23,78 +23,82 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get("/printGuiaServientrega/:printerName/:idVenta/:url", function (req, res) {
-    var idVenta = req.params.idVenta;
-    var url = req.params.url;
-    var printerName = req.params.printerName;
-    var url = req.params.url; 
-    var options = {
-    directory: "./guias/",
-    filename: idVenta+".pdf"
-}
- 
-download(url, options, function(err){
-    if(err){
-      res.send({ 
-        status: '400',
-        msg : 'no se pudo descargar',
-        url: url
-      })      
-    }else{
-    
-      setTimeout(function(){ 
-           //ya se decargo 
-      //ahora modificar 
-      const arichivo = './guias/'+idVenta+'.pdf';
-      var direccionArchivo = path.dirname(arichivo);
-      
-      
-      console.log( path.join('guias',idVenta+'.pdf') );
-        //   const assets = {
-        //   guiaEnvioBytes: fs.readFileSync(path.resolve('./guias/'+idVenta+'.pdf')),
-        // };
-        const pdfDoc = PDFDocumentFactory.load(fs.readFileSync(path.resolve('./guias/'+idVenta+'.pdf')));
-        
-        pdfDoc.removePage(0)
-        const pdfBytes = PDFDocumentWriter.saveToBytes(pdfDoc);
-      
-        const filePath = path.join('guias',idVenta+'.pdf');
-        fs.writeFileSync(filePath, pdfBytes);
-        console.log(`PDF file written to: ${filePath}`);
+  .get("/printGuiaServientrega/:printerName/:idVenta/:url", async function (req, res) {
 
-          const optionsPrinter = {
-            printer: printerName,
-            copies: 3,
-            copie: 3
-          };
-          //maldito maduro
-          console.log(path.resolve('guias', idVenta+'.pdf'));
-          printer
-          .print(path.resolve('guias', idVenta+'.pdf'))
-          .then(function(value) {
-            res.send({ 
-              status: '200',
-              msg : 'ok',
-              url: url
-            })
-          })
-         
-          .catch(function(error) {
-            res.send({ 
-              status: '400',
-              msg : 'ERROR AL IMPRIMIR',
-              error : error,
-              url: url
-            })
-          })
-      
-      }, 2000);
-
+    try {
+      var idVenta = req.params.idVenta;
+      var url = req.params.url;
+      var printerName = req.params.printerName;
+      var url = req.params.url; 
+      var options = {
+      directory: "./guias/",
+      filename: idVenta+".pdf"
+  }
    
-         
-
-      }   
-})
+  download(url, options, async function(err){
+      if(err){
+        res.send({ 
+          status: '400',
+          msg : 'no se pudo descargar',
+          url: url
+        })      
+      }else{
+      
+        setTimeout( async function(){ 
+             //ya se decargo 
+        //ahora modificar 
+        const arichivo = './guias/'+idVenta+'.pdf';
+        var direccionArchivo = path.dirname(arichivo);
+        
+        
+        console.log( path.join('guias',idVenta+'.pdf') );
+          //   const assets = {
+          //   guiaEnvioBytes: fs.readFileSync(path.resolve('./guias/'+idVenta+'.pdf')),
+          // };
+          const pdfDoc = await PDFDocumentFactory.load(fs.readFileSync(path.resolve('./guias/'+idVenta+'.pdf')));
+          
+          pdfDoc.removePage(0)
+          const pdfBytes = PDFDocumentWriter.saveToBytes(pdfDoc);
+        
+          const filePath = path.join('guias',idVenta+'.pdf');
+          fs.writeFileSync(filePath, pdfBytes);
+          console.log(`PDF file written to: ${filePath}`);
+  
+            const optionsPrinter = {
+              printer: printerName,
+              copies: 3,
+              copie: 3
+            };
+            //maldito maduro
+            console.log(path.resolve('guias', idVenta+'.pdf'));
+            printer
+            .print(path.resolve('guias', idVenta+'.pdf'))
+            .then(function(value) {
+              res.send({ 
+                status: '200',
+                msg : 'ok',
+                url: url
+              })
+            })
+           
+            .catch(function(error) {
+              res.send({ 
+                status: '400',
+                msg : 'ERROR AL IMPRIMIR',
+                error : error,
+                url: url
+              })
+            })
+        
+        }, 2000); 
+        }   
+  })
+    }
+    catch(error) {
+      console.error(error);
+   
+    }
+ 
 })
  
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
